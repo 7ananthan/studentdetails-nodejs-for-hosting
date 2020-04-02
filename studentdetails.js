a2e40a1052bfcea4fa41e8c2cfc20f26bcbb2654
7ananthan <7ananthan@gmail.com>
2020-04-02 16:42:02 +0530

    first commit

---
+++ studentdetails.js
@@ -0,0 +1,19 @@
+var express =require('express');
+var bodyParser =require('body-parser');
+
+var app=express();
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('',(req, res )=>{
+
+    var name =req.body.getname;
+    var roll =req.body.getroll;
+    var adminNo=req.body.getadminNo;
+    var college=req.body.getclg;
+
+    res.json(req.body);
+});
+
+app.listen(3000, () => {
+    console.log("server started");
+});
